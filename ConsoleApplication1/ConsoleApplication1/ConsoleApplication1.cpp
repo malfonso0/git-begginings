f15fc9669588c457b4b0c283f6ec758109744f75
malfonsoNeoris <manuel.alfonso@neoris.com>
2021-08-06 09:21:39 -0300

    asasa

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -8,6 +8,7 @@ int main()
 {
     cout << "Hello World!\n";
     cout << "Bienvenidos a LP1" << endl;
+    cout << "Bienvenidos a LP1 de manuel" << endl;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
